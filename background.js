f86605a3128fc8413f4cdeb27c876eca8074e119
con_schy <connorschultze1@gmail.com>
2022-06-14 00:01:09 -0700

    addingCounterRegex

--- background.js
+++ background.js
@@ -45,3 +45,14 @@ chrome.tabs.onRemoved.addListener(tabId => {
         delete data['tab' + tabId];
     });
 });
+
+
+chrome.runtime.onInstalled.addListener(function (object) {
+    let externalUrl = "https://ko-fi.com/globemallow#paypalModal";
+
+    if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+        chrome.tabs.create({ url: externalUrl }, function (tab) {
+            //console.log("New tab launched with http://yoursite.com/");
+        });
+    }
+});
--- performance.js
+++ performance.js
@@ -16,7 +16,7 @@ function blockerFunction(){
 //Analytics Tracker Checker    
 
 const regex1List = [/google\-analytics/,/googletagmanager/,/marketo/,/doubleclick/
-,/scorecardresearch/,/connect\.facebook\.net/,/clarity\.ms/, /chartbeat/,
+,/scorecardresearch/,/connect\.facebook\.net/,/clarity\.ms/,
 /go\-mpulse/,/analytics\.tiktok/,/quantcount/, /snap\.licdn/, /analytics\.similarweb/, /hotjar/, /pardot/,
 /newrelic/, /foresee/, /smetrics\./, /tms\./, /crazyegg/, /krxd\.net/, /boomtrain/
 , /cdn\.turner/, /optimizely/, /bounceexchange/, /visualime/, /tags\.tiqcdn/, /tealiumiq/,
@@ -24,7 +24,7 @@ const regex1List = [/google\-analytics/,/googletagmanager/,/marketo/,/doubleclic
  /thebrighttag/, /s\.btstatic/, /raygun/, /ac\-target/, /demdex/, /utag/, /iperceptions/, /techtarget/, /bizible/,
  /6sc\.co/, /demandbase/, /engagio/, /akamai/, /qualtrics/, /rubiconproject/, /s\.yimg/,
   /cdn\.segment/, /marinsm/, /googlesyndication/, /chartbeat/, /gstatic/, /rlcdn/, /sojern/,
-  /rmtag/, /impactradius\-event/, /bytedance/, /sprig/, /userleap/, /bat\.bing/, /adservice\.google/, /googleadservices/, /geoedge/, /taboola/, /ads\-twitter/, /sleeknote/, /pushcrew/, /onesignal/, /amazon\-adsystem/, /amplify\-outbrain/, /dianomi/, /s\-onetag/, /ads\.pubmatic/, /adroll/, /adnxs/, /crwdcntrl/, /redditstatic/, /ads.\linkedin/, /moatads/, /criteo/, /adlightning/, /turner\.com\/ads/, /quantserve/, /rubiconproject/, /mpulse/];
+  /rmtag/, /impactradius\-event/, /bytedance/, /sprig/, /userleap/, /bat\.bing/, /adservice\.google/, /googleadservices/, /geoedge/, /taboola/, /ads\-twitter/, /sleeknote/, /pushcrew/, /onesignal/, /amazon\-adsystem/, /amplify\-outbrain/, /dianomi/, /s\-onetag/, /ads\.pubmatic/, /adroll/, /adnxs/, /crwdcntrl/, /redditstatic/, /ads.\linkedin/, /moatads/, /criteo/, /adlightning/, /turner\.com\/ads/, /quantserve/, /rubiconproject/, /mpulse/, /\.demex\.net\//, /gateway\.foresee\.com/, /\.scene7/, /googletagservices/, /analytics\.yahoo\.com/, /doubleverify/, /imrworldwide/, /analytics\.twitter/, /adsafeprotected/, /sc\-static/, /bidswitch/, /widgets\-outbrain/, /connect\.facebook\.net/, /igodigital/, /api\-segment/, /ad\-delivery/, /pix\.pub/, /nr\-data/, /c\.lytics/, /indexww/, /p1\.parsely/, /omtrdc/, /curalate/, /richrelevance/, /cquotient/, /api\.drift/, /app\.dynamics/, /pixel\.wp/, /s7\.addthis/, /webtrendslive/, /googleoptimize/, /px\-cloud\.net/, /liveperson/, /tamgrt/, /\.forter/, /\.piwik\.pro/, /3gl\.net/, /btttag/, /crwdcntrl\.net/, /exelator/, /helpscout/, /platform\.twitter/, /px\.ads\.linkedin/, /linkedin\.com\/li\/track/, /tvsquared/, /fullstory/, /powerreviews/, /mouseflow/, /brightcove/, /beacon\.walmart\.com/, /mathtag/, /\/clickstream\//, /\/gauge\/link\//, /\/gauge\/pageview\//, /adsrvr/, /bs\.serving\-sys/, /teads\.tv/, /tvpixel/, /monetate/, /dyntrace/, /custhelp/, /answerscloud/, /yotpo/, /kampyle/, /webcollage/, /salsify\-ecdn/, /ct\.pintrest/, /soptimize\.southwest/, /flashtalking/, /innovid/, /facebook\.com\/tr/, /everesttech/, /r\.turn/, /ads\.linkedin/, /content\.mink/, /siteimproveanalytics/, /newscgp/, /permutive/, /js\+ssdomvar\.js\+generic/, /apps\.bazaarvoice/, /snowplowanalytics/, /6sc\.co/, /bluekai/, /tapad/, /casalemedia/, /usabilla/, /xg4ken/, /api\.amplitude/, /\/b\/ss\//, /quantummetric/, /\/wt\.pl\?/, /spotxchange/, /mookie1/];
 
 
 var scripts = document.head.getElementsByTagName("script");
