6302f0285fb294f9354acef20b342e1ce04483dd
con_schy <connorschultze1@gmail.com>
2022-12-17 23:59:16 -0800

    UpdateUI

--- popup.js
+++ popup.js
@@ -1,59 +1,78 @@
-var tab;
+var tab, chart;
+function updateChartData(x) {
+    //chart.data.labels.push(label);
+    document.getElementById("scoreHTML").innerHTML = x.totalString;
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = [x.totalAnal, x.totalAd];
+    });
+    chart.update();
+}
+function removeData() {
+    //chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.length = 0;
+    });
+    chart.update();
+}
+function makeChart(x) {
+    document.getElementById("scoreHTML").innerHTML = x.totalString;
+    var scriptNum = x.totalTot;
+    let yValues = [];
+
+    switch (scriptNum >= 0) {
+        case scriptNum >= 0:
+            var ctx = document.getElementById('myChart').getContext('2d');
+            yValues = [x.totalAnal, x.totalAd];
+            var barColors = ["#f18931", "#ebdc3d"];
+            break;
+    }
+    
+    /*var analLab = x.totalAnal.toString() + " Analytics"; 
+    var adLab = x.totalAd.toString() + " Ads";*/
+        
+    chart = new Chart(ctx, {
+        type: "doughnut",
+        data: {
+            labels: [' Analytics', ' Ads'],
+            //labels: [analLab, adLab],
+            datasets: [{
+              backgroundColor: barColors,
+              data: yValues
+            }]
+        },
+        options:{
+            plugins:{
+                tooltips:{
+                    yAlign: 'top'
+                }
+            }
+        }
+    });
+}
+
 chrome.tabs.query({
     active: true,currentWindow: true
 }).then(tabs => {
     tab = tabs[0];
     chrome.storage.local.get("tab"+tab.id).then(data => {
         try{
- var x = data["tab"+tab.id];
-            
-            
-document.getElementById("scoreHTML").innerHTML = x.totalString;
-    
-var scriptNum = x.totalTot;     
-
-switch (scriptNum >= 0){
-        
-case scriptNum >= 0:
-var ctx = document.getElementById('myChart').getContext('2d');
-var yValues = [x.totalAnal, x.totalAd];
-var barColors = [
-  "#f18931",
-  "#ebdc3d",
-
-];
-    break;
-
-}
-            
-/*var analLab = x.totalAnal.toString() + " Analytics"; 
-var adLab = x.totalAd.toString() + " Ads";*/
-            
-new Chart(ctx, {
-  type: "doughnut",
-  data: {
-    labels: [' Analytics', ' Ads'],
-    //labels: [analLab, adLab],
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
-  },
-  options:{
-    plugins:{
-     tooltips:{
-         yAlign: 'top'
-     }
-  }
-  }
-});
-    
-        }
-        catch(e) {
+            makeChart(data["tab"+tab.id]);
+        }catch(e) {
             //alert("Page is almost loaded")
         }
-        });
-    
+    });
+});
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (let [changeKey, { oldValue, newValue }] of Object.entries(changes)) {
+    if (changeKey.includes(tab.id)) {
+        if (chart) {
+            updateChartData(newValue);
+        }else {
+            makeChart(newValue);
+        }
+    }
+  }
 });
 
 let pause = document.getElementById("pause");
