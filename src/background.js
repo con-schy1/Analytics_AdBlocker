092326d0a0251c8384758dd1c39e71d361591a32
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2025-12-04 22:05:29 +0530

    removed content script

--- src/background.js
+++ src/background.js
@@ -13,61 +13,6 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
   }
 });
 
-chrome.runtime.onMessage.addListener(async (request, sender) => {
-  // Check if sender.tab exists (it might not for some internal messages)
-  if (!sender.tab) return;
-
-  let data = await chrome.storage.session.get(null);
-
-  // Clean up old data
-  let storedAt = 99999999999999999999999;
-  let toRemove = null;
-
-  if (Object.keys(data).length >= MAXSITES) {
-    for (let z in data) {
-      if (data[z].storedAt < storedAt) {
-        storedAt = data[z].storedAt;
-        toRemove = z;
-      }
-    }
-    if (toRemove) chrome.storage.session.remove(toRemove);
-  }
-
-  // Store new data
-  chrome.storage.session.set({ ["tab" + sender.tab.id]: request });
-
-  // Update Badge Color
-  let colorString = "#ff0d21"; // Default Red
-  if (request.totalTot <= 5)
-    colorString = "#32a852"; // Green
-  else if (request.totalTot <= 14)
-    colorString = "#8ECA2E"; // Light Green
-  else if (request.totalTot <= 24)
-    colorString = "#f4e03a"; // Yellow
-  else if (request.totalTot <= 35) colorString = "#f18931"; // Orange
-
-  chrome.action.setBadgeBackgroundColor({
-    color: colorString,
-    tabId: sender.tab.id,
-  });
-
-  chrome.action.setBadgeText({
-    text: request.totalString,
-    tabId: sender.tab.id,
-  });
-});
-
-// Clear Cache when tab is closed
-chrome.tabs.onRemoved.addListener((tabId) => {
-  chrome.storage.session.get("tab" + tabId).then((data) => {
-    if (data && data["tab" + tabId]) {
-      // Note: 'delete' keyword doesn't work directly on storage.session
-      // We must use chrome.storage.session.remove
-      chrome.storage.session.remove("tab" + tabId);
-    }
-  });
-});
-
 const GLOBAL_JS_RULE_ID = 9000001;
 const GLOBAL_IMG_RULE_ID = 9000002;
 const GLOBAL_VIDEO_RULE_ID = 9000003;
